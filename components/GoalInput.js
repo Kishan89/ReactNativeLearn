8293445e92fee33463f82ac2af785241736e4751
Kishan89 <kishan895737@gmail.com>
2025-06-03 13:31:31 +0530

    final

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,8 +1,15 @@
-import { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { useState } from "react";
+import {
+    View,
+    TextInput,
+    Button,
+    StyleSheet,
+    Modal,
+    Image,
+} from "react-native";
 
 function GoalInput(props) {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
+    const [enteredGoalText, setEnteredGoalText] = useState("");
 
     function goalInputHandler(enteredText) {
         setEnteredGoalText(enteredText);
@@ -10,19 +17,29 @@ function GoalInput(props) {
 
     function addGoalHandler() {
         props.onAddGoal(enteredGoalText);
-        setEnteredGoalText('');
+        setEnteredGoalText("");
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                placeholder="Your course goal!"
-                onChangeText={goalInputHandler}
-                value={enteredGoalText}
-            />
-            <Button title="Add Goal" onPress={addGoalHandler} />
-        </View>
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/images/goal.png')} />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Your course goal!"
+                    onChangeText={goalInputHandler}
+                    value={enteredGoalText}
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="Add Goal" onPress={addGoalHandler} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Cancel" color="red" onPress={props.onCancel} />
+                    </View>
+                </View>
+            </View>
+        </Modal>
     );
 }
 
@@ -31,18 +48,28 @@ export default GoalInput;
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 16,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20,
     },
     textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
-        padding: 8,
+        borderColor: "#cccccc",
+        width: "100%",
+        borderRadius: 6,
+        padding: 16,
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginTop: 16,
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8,
     },
 });
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,13 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
 
 function GoalItem(props) {
-    return (
-        <View style={styles.goalItem}>
+    return (<View style={styles.goalItem}>
+        <Pressable android_ripple={{ color: '#210644' }
+        } onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({ Pressed }) => Pressed && styles.pressedItem}>
             <Text style={styles.goalText}>{props.text}</Text>
-        </View>
+        </Pressable ></View >
+
     );
 }
 
@@ -13,12 +16,15 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
-        backgroundColor: '#5e0acc',
+        backgroundColor: 'rgb(14, 189, 242)',
+    },
+    pressedItem: {
+        opacity: 0.5,
     },
     goalText: {
         color: 'white',
+        padding: 8,
     },
 });
 
